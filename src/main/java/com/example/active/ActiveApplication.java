78c7b41886b24fa70eb103cd297ad88ff68afc1c
“719754462” <719754462@qq.com>
2023-05-23 19:03:09 +0800

    Revert "基本框架"
    This reverts commit e592758c28d512ec22a88798f370bf9eb0a54108.
    
    # Conflicts:
    #	src/main/java/com/example/active/ActiveApplication.java
    #	src/main/java/com/example/active/matlab/matlab_test.java

--- src/main/java/com/example/active/ActiveApplication.java
+++ src/main/java/com/example/active/ActiveApplication.java
@@ -2,7 +2,9 @@ package com.example.active;
 
 import com.example.active.activemq.Queue_Consume;
 import com.example.active.activemq.Queue_Produce;
+import com.example.active.matlab.matlab_test;
 import com.example.active.mysql.controller.MysqlController;
+import com.mathworks.toolbox.javabuilder.MWException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +13,14 @@ import javax.jms.JMSException;
 @SpringBootApplication
 public class ActiveApplication {
 
-    public static void main(String[] args) throws JMSException {
+    public static void main(String[] args) throws JMSException, MWException {
 //        Queue_Produce x=new Queue_Produce();
 //        x.produce();
 //        Queue_Consume y=new Queue_Consume();
 //        y.consume();
-        SpringApplication.run(ActiveApplication.class, args);
+//        SpringApplication.run(ActiveApplication.class, args);
+        matlab_test exp =new matlab_test();
+        exp.test();
     }
 
 }
